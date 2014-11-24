7dd4552cedb3f7c77a3ea943ebab65b6157735be
apvanree <alessvanree@gmail.com>
2014-11-24 11:43:28 +0100

    analyze audio

--- Code/FFT/src/AnalyzeAudio.java
+++ Code/FFT/src/AnalyzeAudio.java
@@ -1,8 +1,11 @@
+import java.io.*;
+
 public class AnalyzeAudio {
 
 	static void main(String args[]) {
 		
-		
+		 WavFile wavFile = WavFile.openWavFile(new File("junk.wav"));
+		 wavFile.display();
 		
 	}
 	
